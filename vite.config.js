afc73759a197afd21ebd449021a163c949ac07bc
PRAVEEN200307 <crouselearn@gmail.com>
2024-02-24 23:32:03 -0500

    vite deplot

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         greeting: resolve(__dirname, "greeting.html"),
+        greeting: resolve(__dirname, "./sakura-js/dist/sakura.min.js"),
       },
     },
   },
